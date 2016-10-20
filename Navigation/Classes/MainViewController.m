ed580e5e78d3a2b970dffa95a58ab06421692521
Jerome <jerome@MacBook-Pro-de-Multidevice.local>
2016-10-20 18:23:30 +0100

    First commit

---
+++ Navigation/Classes/MainViewController.m
@@ -0,0 +1,148 @@
+//
+//  MainViewController.m
+//  Navigation
+//
+//  Created by Tammy Coron on 1/19/13.
+//  Copyright (c) 2013 Tammy L Coron. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "CenterViewController.h"
+#import "LeftPanelViewController.h"
+
+#define CENTER_TAG 1
+#define LEFT_PANEL TAG 2
+
+@interface MainViewController () <CenterViewControllerDelegate>
+
+@property (nonatomic, strong) CenterViewController      *centerViewController;
+@property (nonatomic, strong) LeftPanelViewController   *leftViewController;
+@property (nonatomic, assign) BOOL                      showingLeftPanel;
+
+@end
+
+@implementation MainViewController
+
+#pragma mark -
+#pragma mark View Did Load/Unload
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self setupView];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+#pragma mark -
+#pragma mark View Will/Did Appear
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+}
+
+#pragma mark -
+#pragma mark View Will/Did Disappear
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+#pragma mark -
+#pragma mark Setup View
+
+- (void)setupView
+{
+    // setup center view
+    self.centerViewController = [[CenterViewController alloc] initWithNibName:@"CenterViewController" bundle:nil];
+    self.centerViewController.view.tag = CENTER_TAG;
+    self.centerViewController.delegate = self;
+    
+    [self.view addSubview:self.centerViewController.view];
+    [self addChildViewController:_centerViewController];
+    
+    [_centerViewController didMoveToParentViewController:self];
+}
+
+- (void)showCenterViewWithShadow:(BOOL)value withOffset:(double)offset
+{
+}
+
+- (void)resetMainView
+{
+}
+
+- (UIView *)getLeftView
+{
+    
+    
+}
+
+- (UIView *)getRightView
+{     
+    UIView *view = nil;
+    return view;
+}
+
+#pragma mark -
+#pragma mark Swipe Gesture Setup/Actions
+
+#pragma mark - setup
+
+- (void)setupGestures
+{
+}
+
+-(void)movePanel:(id)sender
+{
+}
+
+#pragma mark -
+#pragma mark Delegate Actions
+
+- (void)movePanelLeft // to show right panel
+{
+}
+
+- (void)movePanelRight // to show left panel
+{
+}
+
+- (void)movePanelToOriginalPosition
+{    
+}
+
+#pragma mark -
+#pragma mark Default System Code
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
